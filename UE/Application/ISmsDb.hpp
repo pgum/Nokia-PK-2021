66fb5d3dafd97a495cfaac091ee68cabc8fdf55c
Robert Kozik <rkozik98@gmail.com>
2021-05-12 19:54:24 +0200

    review changes
    Co-authored-by: Tomasz Kot <tomasz.kot13@gmail.com>

--- UE/Application/ISmsDb.hpp
+++ UE/Application/ISmsDb.hpp
@@ -11,10 +11,6 @@ class ISmsDb
 public:
     virtual ~ISmsDb() = default;
 
-    //wszystkie sms dla ue
-    //jeden specyficzny sms
-    //dodanie sms
-
     virtual std::vector<Sms> getAll() = 0;
     virtual Sms* getOne(int id) = 0;
     virtual void addOne(Sms sms) = 0;
