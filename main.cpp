31cf0106eb51d266708b5acb278b60b14ae76f9c
mikgaw <mikolaj.gawlas1@gmail.com>
2021-05-20 15:08:04 +0200

    update

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include "Application.hpp"
+#include "ApplicationEnvironmentFactory.hpp"
+#include "Ports/BtsPort.hpp"
+#include "Ports/UserPort.hpp"
+#include "Ports/TimerPort.hpp"
+#include "Ports/SmsDbPort.hpp"
+
+int main(int argc, char* argv[])
+{
+    using namespace ue;
+    using namespace std::chrono_literals;
+
+    auto appEnv = ue::createApplicationEnvironment(argc, argv);
+    auto& logger = appEnv->getLogger();
+    auto& tranport = appEnv->getTransportToBts();
+    auto& gui = appEnv->getUeGui();
+    auto phoneNumber = appEnv->getMyPhoneNumber();
+
+    SmsDb db;
+    SmsDb db_w;
+    BtsPort bts(logger, tranport, phoneNumber);
+    UserPort user(logger, gui, phoneNumber, db, db_w);
+    TimerPort timer(logger);
+    Application app(phoneNumber, logger, bts, user, timer, db, db_w);
+    bts.start(app);
+    user.start(app);
+    timer.start(app);
+    appEnv->startMessageLoop();
+    bts.stop();
+    user.stop();
+    timer.stop();
+}
+
